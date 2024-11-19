8c6658f6495756c9f70892b1f4e37bf469c4e1e7
Praveen Suthar <Sutharpraveen234@gmail.com>
2024-11-19 22:50:04 +0530

    dfasdf
    (cherry picked from commit cc5d9f4fa46ab219b3640a6064a591878ed9076c)

---
+++ lib/screen/nnnn/fsdf.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class dfdfs extends StatefulWidget {
+  const dfdfs({super.key});
+
+  @override
+  State<dfdfs> createState() => _dfdfsState();
+}
+
+class _dfdfsState extends State<dfdfs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(
+      children: [
+        Text("sfsdfsd")
+      ],
+    ),);
+  }
+}
