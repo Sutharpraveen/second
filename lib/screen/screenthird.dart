51507a2f27f20a3335bd7ee514982b2fb45fa8c6
Praveen Suthar <Sutharpraveen234@gmail.com>
2024-11-21 22:12:56 +0530

    Update screenthird.dart

---
+++ lib/screen/screenthird.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+
+class Screenthird extends StatefulWidget {
+  const Screenthird({super.key});
+
+  @override
+  State<Screenthird> createState() => _ScreenthirdState();
+}
+
+class _ScreenthirdState extends State<Screenthird> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("data"),),
+      body: Column(
+        children: [
+
+          Stack(
+            children: [],
+          ),
+
+
+          ListView(
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+}
